6270522cda34b413c13e7b96e01f3ec5a98bb588
Adil Hassan <adil27jan@outlook.com>
2022-07-24 14:05:43 +0400

    repository done

---
+++ AdventureworksAPI/AdventureworksAPI.api/Repositories/IPersonrepository.cs
@@ -0,0 +1,9 @@
+﻿using AdventureworksAPI.api.Models.Domain;
+
+namespace AdventureworksAPI.api.Repositories
+{
+    public interface IPersonrepository
+    {
+        IEnumerable<mPerson> GetAll();
+    }
+}
---
+++ AdventureworksAPI/AdventureworksAPI.api/Repositories/PersonRepository.cs
@@ -0,0 +1,19 @@
+﻿using AdventureworksAPI.api.Data;
+using AdventureworksAPI.api.Models.Domain;
+
+namespace AdventureworksAPI.api.Repositories
+{
+    public class PersonRepository : IPersonrepository
+    {
+        private readonly AdventurewalksDBContext adventurewalksDBContext;
+        public PersonRepository(AdventurewalksDBContext adventurewalksDBContext)
+        {
+                this.adventurewalksDBContext = adventurewalksDBContext;
+        }
+        public IEnumerable<mPerson> GetAll()
+        {
+            return adventurewalksDBContext.Person.ToList();
+        }
+    }
+}
+    
